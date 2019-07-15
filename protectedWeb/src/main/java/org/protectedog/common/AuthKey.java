4b07d1c22e4d31dc5930a7106a8f012d482094d7
alfides <alfides@naver.com>
2019-07-15 17:06:56 +0900

    Jintae : 20190715 16:53 1. ID중복체크 Ajax처리 완료 2. Authkey.java(휴대폰인증 인증번호 난수생성처리) 생성 3. 추가정보기입 - 휴대폰인증 Logic 구현 및 Test 완료 4. Kakao 회원연동 및 로그아웃 구현 완료 5. SQL - StoryFunding Table 일부 수정

--- protectedWeb/src/main/java/org/protectedog/common/AuthKey.java
+++ protectedWeb/src/main/java/org/protectedog/common/AuthKey.java
@@ -3,7 +3,7 @@ package org.protectedog.common;
 import java.util.Random;
 
 public class AuthKey{
-    private int certNumLength = 6;
+    private int certNumLength = 5;
     
     public String excuteGenerate() {
         Random random = new Random(System.currentTimeMillis());
