9133615347e1a51905ba33a7b55fcff3470906d3
alfides <alfides@naver.com>
2019-07-18 11:19:43 +0900

    Jintae : 20190718 11:17 1. ReportController - addReport, listReport, getReport, updateReport 구현 2. addReportView, listReportView, getReportView.jsp 생성 3. SQL문 일부 수정 4. ReportMapper.xml 일부 수정 5. Report 파일업로드 경로 FIleUploadRestController.java 및 common.properties에 추가

---
+++ protectedWeb/WebContent/report/getReportView.jsp
@@ -0,0 +1,134 @@
+<%@ page contentType="text/html; charset=EUC-KR"%>
+<%@ page pageEncoding="EUC-KR"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<!--  meta  -->
+<meta charset="EUC-KR">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--  bootstrap CDN  -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!--  bootstrap Dropdown CSS & JS  -->
+<!-- <link href="/resources/css/others/animate.css" rel="stylesheet"> -->
+<script type="text/javascript">
+	$(function() {
+
+		$("button:contains('처리')").on("click", function() {
+			self.location = "/report/updateReport?reportNo="+${ report.reportNo }+"&reportStatus=1&delCode=0"
+		});
+
+		$("button:contains('삭제')").on(
+				"click",
+				function() {
+
+					var result = confirm("정말 삭제 하시겠습니까?");
+
+					if (result) {
+						$("form[name='info']").attr("method", "POST").attr(
+								"action", "/info/delInfo").attr("enctype",
+								"multipart/form-data").submit();
+					}
+
+				});
+	});
+</script>
+<style type="text/css">
+
+body {
+	position: relative;
+}
+.container {
+	font-size: 14px;
+}
+
+.content {
+	text-align: left;
+	width: 720px;
+	min-height: 400px;
+}
+
+.button {
+	position: relative;
+}
+
+.goto-here {
+	padding: 0px;
+}
+</style>
+</head>
+
+<body>
+
+	<jsp:include page="/layout/toolbar.jsp"></jsp:include>
+		
+	<div class="container">
+		
+		<form name="info">
+			<input type="hidden" name="reportNo" value="${report.reportNo}" />
+
+			<div class="row" style="position: relative; height: 25px;">
+				<div class="col-md-8"
+					style="position: absolute; left: 0px; bottom: 0px;">신고자 | ${ report.reporterId }</div>
+							
+				<div class="col-md-4" align="right"
+					style="position: absolute; right: 0px; bottom: 0px;">신고유형 | ${ report.reportCategory }</div>
+			</div>
+			<p />
+			
+			<hr />
+			
+			<div class="row" style="position: relative; height: 25px;">
+				<div class="col-md-8"
+					style="position: absolute; left: 0px; bottom: 0px;">신고대상 | ${ report.reportedId }</div>
+							
+				<div class="col-md-4" align="right"
+					style="position: absolute; right: 0px; bottom: 0px;">
+					신고일시 | <fmt:formatDate value="${ report.reportDate }" pattern="yyyy년 MM월 dd일"/>
+					</div>
+			</div>
+			<p />
+					
+			<hr />
+
+			<div class="content">${report.reportContent}</div>
+			<hr />
+			<hr />
+			<div class="content">
+				<img src="../resources/file/fileReport/${report.file1}">
+			</div>
+			<c:if test="${ report.file2 != null }">
+				<div class="content">${report.file2 }</div>
+			</c:if>
+			<c:if test="${ report.file3 != null }">
+				<div class="content">${report.file3 }</div>
+			</c:if>
+			<hr />
+			<hr />
+		</form>
+
+		<div class="button" align="right">
+			<button>처리</button>
+			<button>삭제</button>
+		</div>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+	</div>
+		
+		<jsp:include page="/layout/footer.jsp"></jsp:include>
+</body>
+</html>
--- protectedWeb/src/test/java/org/protectedog/service/report/test/ReportTest.java
+++ protectedWeb/src/test/java/org/protectedog/service/report/test/ReportTest.java
@@ -34,127 +34,127 @@ public class ReportTest {
 	@Qualifier("reportServiceImpl")
 	private ReportService reportService;
 
-	//@Test
-	public void testAddReport() throws Exception {
-		
-		Report report=new Report();
-		report.setReportNo(10000);
-		
-		User reporter=new User();
-		reporter.setId("user01");
-		report.setReporter(reporter);
-		
-		User reported=new User();
-		reported.setId("user02");
-		report.setReported(reported);
-		
-		report.setReportCategory("비속어사용");
-		report.setReportContent("이색기 순 나쁜색기래요");
-		report.setReportStatus(0);
-		
-		Date reportDate=new Date();
-		reportDate.setYear(1999);
-		reportDate.setMonth(05);
-		reportDate.setDate(01);
-		report.setReportDate(reportDate);
-		
-		report.setFile1("abc.jpg");
-		report.setFile2(null);
-		report.setFile3(null);
-		
-		reportService.addReport(report);
-		
-		report=reportService.getReport(10000);
-		
-		System.out.println(report);
-
-	}
-	
-	//@Test
-	public void testGetReport() throws Exception {
-		
-		Report report=new Report();
-		report.setReportNo(10001);
-		
-		User reporter=new User();
-		reporter.setId("user02");
-		report.setReporter(reporter);
-		
-		User reported=new User();
-		reported.setId("user03");
-		report.setReported(reported);
-		
-		report.setReportCategory("풍기문란");
-		report.setReportContent("음란마귀가 가득 들어있는 게로구나");
-		report.setReportStatus(0);
-		
-		Date reportDate=new Date();
-		reportDate.setYear(2019);
-		reportDate.setMonth(05);
-		reportDate.setDate(01);
-		report.setReportDate(reportDate);
-		
-		report.setFile1("def.jpg");
-		report.setFile2(null);
-		report.setFile3(null);
-		
-		reportService.addReport(report);
-		
-		report=reportService.getReport(10001);
-		
-		System.out.println(report);
-
-	}
-	
-	//@Test
-	 public void testUpdateReport() throws Exception{
-		 
-			
-		Report report=reportService.getReport(10001);
-		
-		report.setReportStatus(1);
-		report.setDelCode('0');
-		
-		reportService.updateReportStatus(report);
-		
-		report=reportService.getReport(10001);
-		
-		System.out.println(report);
-		
-	}
-
-	
-
-	 @Test
-	 public void testGetReportListAll() throws Exception{
-		 
-	 	Search search = new Search();
-	 	search.setCurrentPage(1);
-	 	search.setPageSize(3);
-	 	Map<String,Object> map = reportService.getReportList(search);
-	 	
-	 	List<Object> list = (List<Object>)map.get("list");
-	 	
-	 	System.out.println(list);
-	 	
-	 	Integer totalCount = (Integer)map.get("totalCount");
-	 	System.out.println(totalCount);
-	 	
-	 	System.out.println("=======================================");
-	 	
-	 	search.setCurrentPage(1);
-	 	search.setPageSize(3);
-	 	search.setSearchCondition("0");
-	 	search.setSearchKeyword("");
-	 	map = reportService.getReportList(search);
-	 	
-	 	list = (List<Object>)map.get("list");
-	
-	 	
-	 	System.out.println(list);
-	 	
-	 	totalCount = (Integer)map.get("totalCount");
-	 	System.out.println(totalCount);
-	 }
+//	//@Test
+//	public void testAddReport() throws Exception {
+//		
+//		Report report=new Report();
+//		report.setReportNo(10000);
+//		
+//		User reporter=new User();
+//		reporter.setId("user01");
+//		report.setReporter(reporter);
+//		
+//		User reported=new User();
+//		reported.setId("user02");
+//		report.setReported(reported);
+//		
+//		report.setReportCategory("비속어사용");
+//		report.setReportContent("이색기 순 나쁜색기래요");
+//		report.setReportStatus(0);
+//		
+//		Date reportDate=new Date();
+//		reportDate.setYear(1999);
+//		reportDate.setMonth(05);
+//		reportDate.setDate(01);
+//		report.setReportDate(reportDate);
+//		
+//		report.setFile1("abc.jpg");
+//		report.setFile2(null);
+//		report.setFile3(null);
+//		
+//		reportService.addReport(report);
+//		
+//		report=reportService.getReport(10000);
+//		
+//		System.out.println(report);
+//
+//	}
+//	
+//	//@Test
+//	public void testGetReport() throws Exception {
+//		
+//		Report report=new Report();
+//		report.setReportNo(10001);
+//		
+//		User reporter=new User();
+//		reporter.setId("user02");
+//		report.setReporter(reporter);
+//		
+//		User reported=new User();
+//		reported.setId("user03");
+//		report.setReported(reported);
+//		
+//		report.setReportCategory("풍기문란");
+//		report.setReportContent("음란마귀가 가득 들어있는 게로구나");
+//		report.setReportStatus(0);
+//		
+//		Date reportDate=new Date();
+//		reportDate.setYear(2019);
+//		reportDate.setMonth(05);
+//		reportDate.setDate(01);
+//		report.setReportDate(reportDate);
+//		
+//		report.setFile1("def.jpg");
+//		report.setFile2(null);
+//		report.setFile3(null);
+//		
+//		reportService.addReport(report);
+//		
+//		report=reportService.getReport(10001);
+//		
+//		System.out.println(report);
+//
+//	}
+//	
+//	//@Test
+//	 public void testUpdateReport() throws Exception{
+//		 
+//			
+//		Report report=reportService.getReport(10001);
+//		
+//		report.setReportStatus(1);
+//		report.setDelCode('0');
+//		
+//		reportService.updateReportStatus(report);
+//		
+//		report=reportService.getReport(10001);
+//		
+//		System.out.println(report);
+//		
+//	}
+//
+//	
+//
+//	 @Test
+//	 public void testGetReportListAll() throws Exception{
+//		 
+//	 	Search search = new Search();
+//	 	search.setCurrentPage(1);
+//	 	search.setPageSize(3);
+//	 	Map<String,Object> map = reportService.getReportList(search);
+//	 	
+//	 	List<Object> list = (List<Object>)map.get("list");
+//	 	
+//	 	System.out.println(list);
+//	 	
+//	 	Integer totalCount = (Integer)map.get("totalCount");
+//	 	System.out.println(totalCount);
+//	 	
+//	 	System.out.println("=======================================");
+//	 	
+//	 	search.setCurrentPage(1);
+//	 	search.setPageSize(3);
+//	 	search.setSearchCondition("0");
+//	 	search.setSearchKeyword("");
+//	 	map = reportService.getReportList(search);
+//	 	
+//	 	list = (List<Object>)map.get("list");
+//	
+//	 	
+//	 	System.out.println(list);
+//	 	
+//	 	totalCount = (Integer)map.get("totalCount");
+//	 	System.out.println(totalCount);
+//	 }
  
 }
